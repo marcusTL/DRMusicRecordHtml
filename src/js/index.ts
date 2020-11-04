725c03c3aed66747e16f2af50a40197ec0434c93
Thomas (Noit) <tba@live.dk>
2020-11-04 13:49:15 +0100

    added Update functionality

--- src/js/index.ts
+++ src/js/index.ts
@@ -30,7 +30,15 @@ new Vue({
         searchOption: "",
         deleteTitle: "",
         deleteArtist: "",
-        deleteSuccessful: false
+        deleteSuccessful: false,
+        updateTitle: "",
+        updateArtist: "",
+        updateMessage: "",
+        updateMusicRecord: {
+            title: "", artist: "", durationInSeconds: 0,
+            yearOfPublication: 0, isCertifiedPlatinum: false
+        },
+        updateSuccessful: false
     },
     methods: {
         GetAll(): void {
@@ -56,32 +64,44 @@ new Vue({
                     console.log(error.message);
                 });
         },
-        AddMusicRecord(): void{
-            axios.post<IMusicRecord>(baseURL,this.addMusicRecord)
-            .then((Response: AxiosResponse)=>{
-                console.log(Response.data);
-                console.log(this.addMusicRecord);
-                this.addSuccessful = true;
-            })
-            .catch((error: AxiosError)=>{
-                console.log(this.addMusicRecord);
-                console.log(error.message);
-            })
+        AddMusicRecord(): void {
+            axios.post<IMusicRecord>(baseURL, this.addMusicRecord)
+                .then((Response: AxiosResponse) => {
+                    console.log(Response.data);
+                    console.log(this.addMusicRecord);
+                    this.addSuccessful = true;
+                })
+                .catch((error: AxiosError) => {
+                    console.log(this.addMusicRecord);
+                    console.log(error.message);
+                })
         },
-        
-        //?artist={artist}&title={title}
-        DeleteMusicRecord(deleteArtist: string, deleteTitle: string): void{
-            let deleteQuery = `?artist=${this.deleteArtist}&title=${this.deleteTitle}` ;
-            axios.delete<IMusicRecord>(baseURL + deleteQuery)
-            .then((Response: AxiosResponse)=>{
-                console.log(Response.data);
-                console.log("Deleted: " + this.deleteArtist + " - " + this.deleteTitle);
-                this.deleteSuccessful = true;
-            })
-            .catch((error: AxiosError)=>{
-                console.log("Deleted: " + this.deleteArtist + " - " + this.deleteTitle);
-                console.log(error.message);
-        }
 
+        //?artist={artist}&title={title}
+        DeleteMusicRecord(deleteArtist: string, deleteTitle: string): void {
+            let deleteQuery = `?artist=${this.deleteArtist}&title=${this.deleteTitle}`;
+            axios.delete<IMusicRecord>(baseURL + deleteQuery)
+                .then((Response: AxiosResponse) => {
+                    console.log(Response.data);
+                    console.log("Deleted: " + this.deleteArtist + " - " + this.deleteTitle);
+                    this.deleteSuccessful = true;
+                })
+                .catch((error: AxiosError) => {
+                    console.log("Something went wrong during deletion...");
+                    console.log(error.message);
+                })
+        },
+        UpdateMusicRecord(updateArtist: string, updateTitle: string): void {
+            let updateQuery = `?artist=${updateArtist}&title=${updateTitle}`;
+            axios.put<IMusicRecord>(baseURL + updateQuery, this.updateMusicRecord)
+                .then((response: AxiosResponse) => {
+                    console.log(response.data);
+                    this.updateSuccessful = true;
+                    this.updateMessage = `Updated: ${updateArtist} - ${updateTitle} => ${this.updateMusicRecord.artist} - ${this.updateMusicRecord.title}`;
+                })
+                .catch((error: AxiosError) => {
+                    console.log(error.message);
+                })
+        }
     }
 })
